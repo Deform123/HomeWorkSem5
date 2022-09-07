9243f856956f8cb50bfc6efe467119c23e20e56a
IvanKovalev <deformgame@gmail.com>
2022-09-07 14:45:37 +0300

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Clear();
+
+Console.WriteLine("Введите воличество чисел в массиве : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [n];
+int sum = 0;
+int i = 1;
+
+while (i < array.Length)
+    {
+        array[i] = new Random().Next(0, 10);
+        Console.Write($"{array [i] + " "}");
+        
+        if (i % 2 != 0)
+            {
+                sum = sum + array[i];
+                i++;
+            }
+        else i++;
+    }
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов массива с нечетным индексом равна {sum}.");
