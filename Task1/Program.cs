76dc27f707c7f05ba203b77c8a6c5b6acceaa31f
IvanKovalev <deformgame@gmail.com>
2022-09-07 14:23:29 +0300

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+
+Console.WriteLine("Введите воличество чисел в массиве : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [n];
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        Console.Write($"{array [i] + " "}");
+        if (array[i] % 2 == 0)
+            result = result + 1;
+        
+    }
+ Console.WriteLine();
+ Console.WriteLine($"Количество четных элементов в массиве равно {result}.");
+
